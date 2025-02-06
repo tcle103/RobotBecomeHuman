e8d65150790dafb3673760fbbc7af41818e2f77f
Shredderman64 <31966305+Shredderman64@users.noreply.github.com>
2025-02-06 03:51:30 -0800

    Added respawn action to input map

--- Assets/Scripts/Respawn.cs
+++ Assets/Scripts/Respawn.cs
@@ -1,20 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Respawn : MonoBehaviour
 {
     public GameObject spawnPoint;
+
+    InputAction respawnAction;
     // Start is called before the first frame update
     void Start()
     {
-        
+        respawnAction = InputSystem.actions.FindAction("Respawn");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (spawnPoint && respawnAction.triggered) {
+            this.transform.position = spawnPoint.transform.position;
+        }
     }
 
     void OnTriggerEnter2D(Collider2D collision) {
