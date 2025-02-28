655613c3a930c318cbb2330bb0b531bfb3c2f8da
Shredderman64 <31966305+Shredderman64@users.noreply.github.com>
2025-02-27 23:35:41 -0800

    Added highlight methods to interactable interface

--- Assets/Scripts/ButtonInteract.cs
+++ Assets/Scripts/ButtonInteract.cs
@@ -21,4 +21,10 @@ public class ButtonInteract: MonoBehaviour, IInteractable
     {
         action.Invoke();
     }
+
+    public void Highlight()
+    {}
+
+    public void Unhighlight()
+    {}
 }
--- Assets/Scripts/IInteractable.cs
+++ Assets/Scripts/IInteractable.cs
@@ -5,4 +5,6 @@ using UnityEngine;
 public interface IInteractable
 {
     void Interact();
+    void Highlight();
+    void Unhighlight();
 }
