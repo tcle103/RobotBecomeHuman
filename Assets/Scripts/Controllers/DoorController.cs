55b8ecaca1c0e4ce87c71da1285550d03a08858a
Shredderman64 <31966305+Shredderman64@users.noreply.github.com>
2025-03-05 05:47:59 -0800

    Added button and door sound effects

--- Assets/Scripts/Controllers/DoorController.cs
+++ Assets/Scripts/Controllers/DoorController.cs
@@ -7,12 +7,14 @@ public class DoorController : MonoBehaviour
     [SerializeField] private Color _openColor, _closedColor;
     SpriteRenderer _doorRenderer;
     Collider2D _doorCollider;
+    AudioSource _audioSource;
     bool isOpen;
     // Start is called before the first frame update
     void Awake()
     {
         _doorCollider = GetComponent<Collider2D>();
         _doorRenderer = GetComponent<SpriteRenderer>();
+        _audioSource = GetComponent<AudioSource>();
         _doorRenderer.color = _closedColor;
     }
 
@@ -33,12 +35,14 @@ public class DoorController : MonoBehaviour
     public void Open() {
         _doorCollider.isTrigger = true;
         _doorRenderer.color = _openColor;
+        if (!isOpen) _audioSource.Play();
         isOpen = true;
     }
 
     public void Close() {
         _doorCollider.isTrigger = false;
         _doorRenderer.color = _closedColor;
+        if (isOpen) _audioSource.Play();
         isOpen = false;
     }
 }
