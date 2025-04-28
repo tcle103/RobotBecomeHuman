7c5d056fa1be4c580abbba965176f05ed69b3c22
Ian-Stentz <ianstentz25@gmail.com>
2025-04-28 16:38:47 -0700

    Added Shell; need to ask about installing spline package

---
+++ Assets/172/Scripts/NPC Behavior/NPCMovementComponent.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D;
+
+public class NPCMovementComponent : MonoBehaviour
+{
+    [SerializeField] private int StartPoint;
+    [SerializeField] private Spline moveSpline; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
