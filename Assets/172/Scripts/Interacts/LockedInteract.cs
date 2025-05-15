4eebb4d46d86feaf048e7f29a9aaa6bb5ad6f5f1
tcle103 <146970839+tcle103@users.noreply.github.com>
2025-05-15 12:25:57 -0700

    door can open when pick up recorder

---
+++ Assets/172/Scripts/Interacts/LockedInteract.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class LockedInteract : MonoBehaviour, IInteractable
+{
+    private PlayerData playerData;
+    [SerializeField] private string keyItem;
+    public UnityEvent action;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerData = GameObject.Find("Player").GetComponent<PlayerData>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Interact()
+    {
+        Dictionary<string, int> currentInventory = playerData.inventoryGet();
+        if (currentInventory.ContainsKey(keyItem))
+        {
+            action.Invoke();
+        }
+    }
+
+    public void Highlight()
+    {}
+
+    public void Unhighlight()
+    {}
+}
