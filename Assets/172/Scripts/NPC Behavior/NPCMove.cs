7f5d5b6cad18b8e66abac3da73ff81ea5f545972
Ian-Stentz <ianstentz25@gmail.com>
2025-06-01 22:19:21 -0700

    testing new movement, saving temp changes here

--- Assets/172/Scripts/NPC Behavior/NPCMove.cs
+++ Assets/172/Scripts/NPC Behavior/NPCMove.cs
@@ -1,6 +1,6 @@
 /* 
- * Last modified by: Tien Le
- * Last modified on: 5/14/25
+ * Last modified by: Ian Stentz
+ * Last modified on: 6/1/2025
  *
  * NPCMove.cs contains functionality for
  * grid-based NPC movement.
@@ -10,7 +10,7 @@
  *
  * Created by: Tien Le
  * Created on: 5/14/25
- * Contributors: Tien Le, Kellum Inglin, Anthony Garcia (Tony)
+ * Contributors: Tien Le, Kellum Inglin, Anthony Garcia (Tony), Ian Stentz
  */
 
 using System.Collections;
@@ -30,10 +30,13 @@ public class NPCMove : MonoBehaviour
     [SerializeField] private List<GameObject> path;
     private int point = 0;
     [SerializeField] private UnityEvent moveCompleteEvent;
+    private Rigidbody rb;
+    //[SerializeField] private float speed = 5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //playerTransform = GameObject.FindWithTag("Player").transform;
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -51,22 +54,21 @@ public class NPCMove : MonoBehaviour
         Debug.Log("moving x");
         Debug.Log(path[point].transform.position.x);
         float moveX = path[point].transform.position.x - transform.position.x;
-        StartCoroutine(MoveLerp(new Vector3(moveX, 0, 0), baseSpeed * Math.Abs(moveX)));
+        MoveLerp(new Vector3(moveX, 0, 0), baseSpeed * Math.Abs(moveX));
         while (transform.position.x != path[point].transform.position.x)
         {
             yield return null;
         }
         Debug.Log("moving y");
         float moveY = path[point].transform.position.y - transform.position.y;
-        StartCoroutine(MoveLerp(new Vector3(0, moveY, 0), baseSpeed * Math.Abs(moveY)));
+        MoveLerp(new Vector3(0, moveY, 0), baseSpeed * Math.Abs(moveY));
         while (transform.position.y != path[point].transform.position.y)
         {
             yield return null;
         }
         Debug.Log("here");
         if (point < path.Count - 1)
-        {
-            
+        {  
             ++point;
             StartCoroutine(Move(path));
         }
@@ -83,22 +85,26 @@ public class NPCMove : MonoBehaviour
 
     }
 
-    private IEnumerator MoveLerp(Vector3 direction, float timeToMove)
+    private void MoveLerp(Vector3 direction, float speed)
     {
-        float elapsedTime = 0;
+        //float elapsedTime = 0;
 
         origPos = transform.position;
         targetPos = origPos + direction;
 
-        while (elapsedTime < timeToMove)
-        {
-            transform.position = Vector3.Lerp(origPos, targetPos, (elapsedTime / timeToMove));
-            elapsedTime += Time.deltaTime;
-            yield return null;
-        }
+        rb.MovePosition(targetPos);
+
+        // while (elapsedTime < timeToMove)
+        // {
+        //     //switch to rb.MovePosition? or split into individual cells and check if player is in the cell before moving into it?
+        //     //transform.position = Vector3.Lerp(origPos, targetPos, (elapsedTime / timeToMove));
+
+        //     //elapsedTime += Time.deltaTime;   
+        //     yield return null;
+        // }
 
         // [4/30/25 Tien] just make sure you are precisely
         // at targetPos at the end of Lerp
-        transform.position = targetPos;
+        //transform.position = targetPos;
     }
 }
